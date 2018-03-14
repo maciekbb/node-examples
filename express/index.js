63efcac433977db0cf53f27060f07f89c8361b46
Maciej Watroba <maciej.watroba@brainly.com>
2018-03-14 19:26:47 +0100

    Add express example

---
+++ express/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/', (req, res) => {
+    console.log(`HEADERS: ${JSON.stringify(req.headers)}`);
+    console.log(`BODY: ${JSON.stringify(req.body, null, 2)}`);
+
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
